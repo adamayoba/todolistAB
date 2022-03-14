40359363e62937049990935fa89b98bacf5c9ed5
Adama BAYO <bayoadama.9@gmail.com>
2022-03-14 17:04:45 +0100

    add route

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AppProvider } from "./context";
-
+import {BrowserRouter} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-     <AppProvider>
+    <BrowserRouter>
       <App />
-    </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
